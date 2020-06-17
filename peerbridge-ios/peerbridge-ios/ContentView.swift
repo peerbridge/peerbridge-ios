e6636abdb485b13cd025b9f26a618a23c6a8b229
PhilippMatthes <github@philippmatth.es>
2020-06-17 12:22:29 +0200

    Add base iOS project

---
+++ peerbridge-ios/peerbridge-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  peerbridge-ios
+//
+//  Created by It's free real estate on 17.06.20.
+//  Copyright © 2020 PeerBridge. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
