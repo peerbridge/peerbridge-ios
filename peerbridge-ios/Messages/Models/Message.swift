2286124c57cf04f44e98ece832a8a6952b45b19f
Felix Kästner <git@felix-kaestner.com>
2021-04-30 11:33:19 +0200

    Update Message.swift

--- peerbridge-ios/Messages/Models/Message.swift
+++ peerbridge-ios/Messages/Models/Message.swift
@@ -4,7 +4,7 @@ import CryptoKit
 
 public protocol Message: Codable {
     /// Messages must denote a type identifier string to avoid decoding ambiguity
-    var typeIdentifier: String { get }
+    var type: String { get }
     
     /// Messages must give a short description for the chat view preview
     var shortDescription: String { get }
